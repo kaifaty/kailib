71579654d71b367efe08d6c9f7a789ff7404a6d6
Kaifat <andry.diego2011@gmail.com>
2021-03-27 15:34:54 +0500

    append cookie set/get/delete functions

--- src/index.ts
+++ src/index.ts
@@ -366,8 +366,48 @@ export function debonce(value: number){
                      return;
                 }
                 lastCall = Date.now();
-                return descriptor.value.call(this, args);
+                return descriptor.value.call(this, ...args);
             }
         };
+    }
+}
+export function setCookie(name: string, value: string, options: any = {}): void {
+    options = {
+        path: '/',
+        "max-age": 84600 * 365,
+        secure: true,
+        // при необходимости добавьте другие значения по умолчанию
+        ...options
+    };
+
+    if (options.expires instanceof Date) {
+        options.expires = options.expires.toUTCString();
+    }
+
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+    for (let optionKey in options) {
+        updatedCookie += "; " + optionKey;
+        let optionValue = options[optionKey];
+        if (optionValue !== true) {
+            updatedCookie += "=" + optionValue;
+        }
+    }
+
+    document.cookie = updatedCookie;
+}
+export function getCookie(name: string): string{
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+export function deleteAllCookies(): void{
+    let cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        let eqPos = cookie.indexOf("=");
+        let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     }
 }
